4b8727a3312b462376e537fc35080253f824b896
lyle-bongani <lylechadya139@gmail.com>
2025-09-23 13:22:30 +0200

    eleventh commit

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   const [showPrivacy, setShowPrivacy] = useState(false);
 
   return (
-    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
+    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 px-2 sm:px-6 lg:px-8">
       <div className="mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
       <div className="flex gap-2">
         <button
